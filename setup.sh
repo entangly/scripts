13d21a61750fd72332e58e5082633f8c4e875e9e
Connor Swain <connor@twizz.com>
2016-05-24 14:32:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-export DJANGO_SETTINGS_MODULE=twizz.base_settings
+export DJANGO_SETTINGS_MODULE=table.local_settings
+export SECRET_KEY="rlvz)vm_)_98@hc)o!gs!p$cnw^s@2-orzz&vi9t-%2eh*_z#)"
 source venv/bin/activate
